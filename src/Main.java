b5459fe33e46e126c8d02c0cbb0e4793c70f7b9f
rachit147852 <so3291500@gmail.com>
2026-04-01 14:39:52 +0530

    UC5

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,22 @@ public class Main {
         System.out.println("==== train Consist Management App====");
         System.out.println("==================");
 
-        List<String> train = new LinkedList<>();
+        Set<String> train = new LinkedHashSet<>();
         train.add("Engine");
         train.add("Sleeper");
-        train.add("AC");
+        //train.add("AC");
         train.add("CARGO");
         train.add("GUARD");
+        train.add("CARGO");
         System.out.println("AFTER ADDING trainS:");
         System.out.println("PASSENGER trainS:"+train);
-        train.add(2,"PANTRY CAR");
-        System.out.println("AFTER ADDING PATRY CAR");
-        System.out.println("PASSENGER trainS:"+train);
-        train.removeFirst();
-        train.removeLast();
-        System.out.println("AFTER REMOVING FIRST AND LAST");
-        System.out.println("PASSENGER trainS:"+train);
+//        train.add(2,"PANTRY CAR");
+//        System.out.println("AFTER ADDING PATRY CAR");
+//        System.out.println("PASSENGER trainS:"+train);
+//        train.removeFirst();
+//        train.removeLast();
+//        System.out.println("AFTER REMOVING FIRST AND LAST");
+//        System.out.println("PASSENGER trainS:"+train);
 
 //
 //        System.out.println("AFTER REMOVING AC CHAIR:");
@@ -28,6 +29,6 @@ public class Main {
 //        System.out.println("PASSENGER trainS:"+train);
 //        System.out.println("CHECKING IF SLEEPER EXISTS:");
 //        System.out.println("trainS:"+train);
-        System.out.println(" UC4 operations completed successfully...");
+        System.out.println(" UC5 operations completed successfully...");
 }
 }
