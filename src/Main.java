96469b4b23b3a0aab760a1d5404108d0fe6186e2
rachit147852 <so3291500@gmail.com>
2026-04-01 13:51:24 +0530

    UC1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-Class Main{
-    public static void main(String[] args){
+import java.util.List;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==================");
+        System.out.println("==== Train Consist Management App====");
+        System.out.println("==================");
+
+        List<String> train = new ArrayList<>();
+        System.out.println("Train initialized successfully");
+        System.out.println("Initial Bogie Count :"+train.size());
+        System.out.println("Initial Train Count:"+train);
+        System.out.println("System ready for operations");
     }
-        }
+
+}
