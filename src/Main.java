c6ddf43cd41710dd747479f5685eb184cb752569
rachit147852 <so3291500@gmail.com>
2026-04-01 14:09:17 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-Class Main{
-    public static void main(String[] args){
+import java.util.List;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==================");
+        System.out.println("==== Train Consist Management App====");
+        System.out.println("==================");
+
+        List<String> train = new ArrayList<>();
+        train.add("SLEEPER");
+        train.add("AC CHAIR");
+        train.add("FIRST CLASS");
+        System.out.println("AFTER ADDING BOGIES:");
+        System.out.println("PASSENGER BOGIES:"+train);
+        System.out.println("AFTER REMOVING AC CHAIR:");
+        train.remove(train.indexOf("AC CHAIR"));
+        System.out.println("PASSENGER BOGIES:"+train);
+        System.out.println("CHECKING IF SLEEPER EXISTS:");
+        System.out.println("CONTAINS SLEEPER? :" + (train.contains("SLEEPER")));
+        System.out.println("UC2 operations completed successfully...");
     }
-        }
+}
