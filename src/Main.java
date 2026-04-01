771633f0759730f9a4f1e44cad6778a6ac9b2d39
rachit147852 <so3291500@gmail.com>
2026-04-01 14:26:43 +0530

    UC3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("==================");
-        System.out.println("==== Train Consist Management App====");
+        System.out.println("==== bogie Consist Management App====");
         System.out.println("==================");
 
-        List<String> train = new ArrayList<>();
-        System.out.println("Train initialized successfully");
-        System.out.println("Initial Bogie Count :"+train.size());
-        System.out.println("Initial Train Count:"+train);
-        System.out.println("System ready for operations");
-    }
-
+        Set<String> bogie = new HashSet<>();
+        bogie.add("BG101");
+        bogie.add("BG102");
+        bogie.add("BG103");
+        bogie.add("BG104");
+        bogie.add("BG101");
+        bogie.add("BG102");
+        System.out.println("AFTER ADDING BOGIES:");
+//        System.out.println("PASSENGER BOGIES:"+bogie);
+//        System.out.println("AFTER REMOVING AC CHAIR:");
+//        bogie.remove(bogie.indexOf("AC CHAIR"));
+//        System.out.println("PASSENGER BOGIES:"+bogie);
+//        System.out.println("CHECKING IF SLEEPER EXISTS:");
+        System.out.println("BOGIES:"+bogie);
+        System.out.println("NOTE:\n DUPLICATES ARE AUTOMATICALLY IGNORED \n UC2 operations completed successfully...");
+}
 }
